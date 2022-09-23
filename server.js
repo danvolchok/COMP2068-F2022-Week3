980f2b7c6e0fb41a15ff5c416e528d1c0b497cf5
Dan Volchok <volchokdan@gmail.com>
2022-09-23 15:55:56 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+// create new express object
+
+const app = express()
+
+// add http request handler
+let hello = (req, res) => {
+    res.writeHead(200)
+    res.write('Hello world')
+    res.end()
+}
+
+// route http requests to our handler function
+app.use(hello)
+
+
+// start express web server
+app.listen(3000)
+console.log('Express is running on Port 3000')
